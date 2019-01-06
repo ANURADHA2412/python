cd8256cc287f8bffa157474909a42e7a07dfd766
ANURADHA2412 <45077926+ANURADHA2412@users.noreply.github.com>
2019-01-06 12:49:29 +0530

    Create age.c

---
+++ age.c
@@ -0,0 +1,9 @@
+x = raw_input("please enter name:")
+y = raw_input("please enter age:")
+z = raw_input("please your gender:")
+
+print "Hello " + x
+print "Your age is " + y
+print "You are " + z
+
+raw_input ("To exit press enter")
